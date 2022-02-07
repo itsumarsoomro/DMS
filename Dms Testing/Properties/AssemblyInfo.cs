91710d4280fac07c634c51ed817fea9d5ae37c4c
itsumarsoomro <umarsoomro76@gmail.com>
2022-02-07 20:55:45 +0000

    commit

---
+++ Dms Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Dms Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Dms Testing")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("47ba8541-ae7e-45a8-8412-71de44a04e54")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DmsClasses/clsDataConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DmsClasses
+{
+    class clsDataConnection
+    {
+        //connection obj used to connect to the database
+        SqlConnection connectionToDB = new SqlConnection();
+        //data adapter use to transfer data to and from the DB
+        SqlDataAdapter dataChannel = new SqlDataAdapter();
+    }
+    set
+    {
+        //set the query results
+        dataTable = value;
+    }
+}// this close namespace
