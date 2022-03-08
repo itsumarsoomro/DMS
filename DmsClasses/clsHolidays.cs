ee86fe55d0cb111f274f84270c7b1ea4e2f3bae9
Mohammad Soomro <P2630030@my365.dmu.ac.uk>
2022-03-08 00:17:01 +0000

    Added testing for holiday class

--- DmsClasses/clsHolidays.cs
+++ DmsClasses/clsHolidays.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace DmsClasses
 {
-     public class clsHolidays
+    public class clsHolidays
     {
-
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public string EmpDepart { get; set; }
+        public string Reason { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
     }
 
- 
+
 }
