0831e0c29ee10eb928fe4164d6f8694065cf5a1a
richardeze123 <P2597140@GWHXX683WIN018W>
2022-03-14 19:40:50 +0000

    Completed week 20 lab

--- DmsFrontOffice/AnExpenses.aspx.cs
+++ DmsFrontOffice/AnExpenses.aspx.cs
@@ -24,4 +24,34 @@ public partial class AnExpenses : System.Web.UI.Page
         //redirect to the viewer page
         Response.Redirect("ExpensesViewer.aspx");
     }
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        //create a instance of new class
+        clsExpenses AnExpenses = new clsExpenses();
+        //varaible to store the primary key
+        Int32 ExpensesNo;
+        //variable to store the result of the find operation
+        Boolean Found = false;
+        //get the primary key entered by the user
+        ExpensesNo = Convert.ToInt32(txtExpensesNo.Text);
+        //find the record
+        Found = AnExpenses.Find(ExpensesNo);
+        //if found true
+        if (Found == true)
+        {
+            //display the values of the properties in the form
+            txtName.Text = AnExpenses.Name;
+            txtCategory.Text = AnExpenses.Category;
+            txtExpenses.Text = AnExpenses.Expenses.ToString();
+        }
+        else
+        {
+            //display error message
+            txtName.Text = "";
+            txtCategory.Text = "";
+            txtExpenses.Text = "";
+            lblError.Text = "Invalid record";
+        }
+    }
 }
